c807d474274beaaa5b816a9ae65de091fd9cce3c
Thomas Ferrone <tferrone@lfdj.com>
2025-06-10 10:40:11 +0200

    Changement du router sur la page clients

--- app/(app)/clients/page.tsx
+++ app/(app)/clients/page.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { generateMockClients } from "@/lib/mockClients";
 import { Search } from "lucide-react";
 
