21876f448567e5e72865704cd21c8b54f1b76019
Thomas Ferrone <thomas.ferrone@fdjunited.com>
2025-06-13 13:15:57 +0200

    Nom de l'app dans le footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
-    return (
-        <footer className="bg-zinc-900 text-zinc-400 text-sm text-center py-4 mt-auto">
-            © {new Date().getFullYear()} MonApp — Tous droits réservés.
-        </footer>
-    );
+  return (
+    <footer className="bg-zinc-900 text-zinc-400 text-sm text-center py-4 mt-auto">
+      © {new Date().getFullYear()} NOVA | FDJ United - DARCS
+    </footer>
+  );
 };
